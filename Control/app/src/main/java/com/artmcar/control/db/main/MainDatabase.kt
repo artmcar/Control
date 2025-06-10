d0b9a1a4140ed626b7bcc7fc6130db5e7f463d72
Artur <makaryan.a.a@edu.mirea.ru>
2025-06-10 21:09:44 +0300

    Well, almost.

--- Control/app/src/main/java/com/artmcar/control/db/main/MainDatabase.kt
+++ Control/app/src/main/java/com/artmcar/control/db/main/MainDatabase.kt
@@ -6,7 +6,7 @@ import androidx.room.Room
 import androidx.room.RoomDatabase
 import androidx.room.TypeConverters
 
-@Database(entities = [MainFields::class], version = 5, exportSchema = false)
+@Database(entities = [MainFields::class], version = 1, exportSchema = false)
 @TypeConverters(Converters::class)
 abstract class MainDatabase: RoomDatabase() {
     abstract fun mainDao(): MainDao
