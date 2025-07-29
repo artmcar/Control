a6df48bf877e6974b7d8983e33027ee69ea2c175
Artur <makaryan.a.a@edu.mirea.ru>
2025-07-29 13:02:24 +0300

    Added the ability to delete an entry from a read fragment

--- Control/app/src/main/java/com/artmcar/control/fragments/ReadFragment.kt
+++ Control/app/src/main/java/com/artmcar/control/fragments/ReadFragment.kt
@@ -1,29 +1,39 @@
 package com.artmcar.control.fragments
 
+import android.app.AlertDialog
+import android.content.Context
 import android.os.Bundle
 import android.text.method.ScrollingMovementMethod
 import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.Toast
+import androidx.lifecycle.ViewModelProvider
 import com.artmcar.control.R
 import com.artmcar.control.databinding.FragmentReadBinding
 import com.artmcar.control.db.main.MainFields
+import com.artmcar.control.db.main.MainViewModel
 
 class ReadFragment : Fragment() {
     private var _binding: FragmentReadBinding? = null
     private val binding get() = _binding!!
 
+    private lateinit var mainViewModel: MainViewModel
+    private var item: MainFields? = null
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View {
         _binding = FragmentReadBinding.inflate(inflater, container, false)
+
+        mainViewModel = ViewModelProvider(this).get(MainViewModel::class.java)
+
         val context = binding.root.context
-        val item = arguments?.getSerializable("item_data") as? MainFields
-        val date_array = binding.root.context.resources.getStringArray(R.array.months)
-        val currency_array = binding.root.context.resources.getStringArray(R.array.currency)
+        item = arguments?.getSerializable("item_data") as? MainFields
+        val date_array = context.resources.getStringArray(R.array.months)
+        val currency_array = context.resources.getStringArray(R.array.currency)
 
         item?.let {
             val is_expense = it.operation == "Expense"
@@ -51,6 +61,7 @@ class ReadFragment : Fragment() {
                 if (is_expense){ context.getString(R.string.type_expense_colon)}
                 else {context.getString(R.string.type_income_colon)}
 
+
         }
 
         return binding.root
@@ -58,10 +69,36 @@ class ReadFragment : Fragment() {
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
+        binding.readToolbar.inflateMenu(R.menu.read_menu)
         binding.readToolbar.setNavigationIcon(R.drawable.ic_back_arrow)
         binding.readToolbar.setNavigationOnClickListener {
             requireActivity().onBackPressedDispatcher.onBackPressed()
         }
+        binding.readToolbar.setOnMenuItemClickListener {
+            when(it.itemId){
+                R.id.action_delete_from_read -> {
+                    item?.let {
+                        deleteExpense(requireContext(), it)
+                    }
+                    true
+                }
+                else -> false
+            }
+        }
+    }
+
+    private fun deleteExpense(context: Context, mainFields: MainFields){
+        val builder = AlertDialog.Builder(context)
+        builder.setPositiveButton(R.string.allert_yes){ _, _ ->
+            mainViewModel.deleteExpense(mainFields)
+            requireActivity().onBackPressedDispatcher.onBackPressed()
+            val message_for_one = context.getString(R.string.successfully_deleted)
+            Toast.makeText(context, message_for_one, Toast.LENGTH_LONG).show()
+        }
+        builder.setNegativeButton(R.string.allert_no){ _, _->}
+        builder.setTitle(R.string.delete_question)
+        builder.setMessage(R.string.are_you_sure_to_delete)
+        builder.create().show()
     }
 
 
